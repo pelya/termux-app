c47d1ecf9a35036fc8963595a806ebcb06dddb6b
Sergii Pylypenko <x.pelya.x@gmail.com>
2025-09-17 14:42:18 +0300

    Bmup version

--- signBundle.sh
+++ signBundle.sh
@@ -2,11 +2,9 @@
 # Set path to your Android keystore and your keystore alias here, or put them in your environment
 [ -z "$ANDROID_UPLOAD_KEYSTORE_FILE" ] && ANDROID_UPLOAD_KEYSTORE_FILE=$HOME/.android/debug.keystore
 [ -z "$ANDROID_UPLOAD_KEYSTORE_ALIAS" ] && ANDROID_UPLOAD_KEYSTORE_ALIAS=androiddebugkey
-PASS="-storepass android"
-[ -n "$ANDROID_UPLOAD_KEYSTORE_PASS" ] && PASS="-storepass:env ANDROID_UPLOAD_KEYSTORE_PASS"
-[ -n "$ANDROID_UPLOAD_KEYSTORE_PASS_FILE" ] && PASS="-storepass:file $ANDROID_UPLOAD_KEYSTORE_PASS_FILE"
-
-set -x
+PASS="--ks-pass pass:android"
+[ -n "$ANDROID_UPLOAD_KEYSTORE_PASS" ] && PASS="--ks-pass env:ANDROID_UPLOAD_KEYSTORE_PASS"
+[ -n "$ANDROID_UPLOAD_KEYSTORE_PASS_FILE" ] && PASS="--ks-pass file:$ANDROID_UPLOAD_KEYSTORE_PASS_FILE"
 
 APPNAME=termux-app
 APPVER=`grep -m 1 versionName app/build.gradle | sed 's/versionName//' | tr -d '" '`
@@ -19,7 +17,7 @@ cd app/build/outputs/bundle/releaseWithDebugInfo || exit 1
 cp -f app-releaseWithDebugInfo.aab ../../../../../$APPNAME-$APPVER.aab || exit 1
 # Sign with the new certificate
 echo Using keystore $ANDROID_UPLOAD_KEYSTORE_FILE
-stty -echo
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $ANDROID_UPLOAD_KEYSTORE_FILE $PASS ../../../../../$APPNAME-$APPVER.aab $ANDROID_UPLOAD_KEYSTORE_ALIAS || exit 1
-stty echo
-echo
+#jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $ANDROID_UPLOAD_KEYSTORE_FILE $PASS ../../../../../$APPNAME-$APPVER.aab $ANDROID_UPLOAD_KEYSTORE_ALIAS || exit 1
+apksigner sign --min-sdk-version 26 \
+ --ks $ANDROID_UPLOAD_KEYSTORE_FILE $PASS --ks-key-alias $ANDROID_UPLOAD_KEYSTORE_ALIAS \
+ ../../../../../$APPNAME-$APPVER.aab  || exit 1
