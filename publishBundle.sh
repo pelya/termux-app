edaed56476ab501bed14ff677c5cbc10d013069a
Sergii Pylypenko <x.pelya.x@gmail.com>
2025-09-26 12:29:15 +0300

    Play Store rejects bundles bigger than 4000MB

--- publishBundle.sh
+++ publishBundle.sh
@@ -4,5 +4,4 @@ rm -rf tmp
 mkdir -p tmp
 export GRADLE_OPTS="-Djava.io.tmpdir=`pwd`/tmp"
 
-./gradlew --stop
 ./gradlew publishReleaseBundle 2>&1 | tee publishBundle.log
--- signBundle.sh
+++ signBundle.sh
@@ -2,22 +2,28 @@
 # Set path to your Android keystore and your keystore alias here, or put them in your environment
 [ -z "$ANDROID_UPLOAD_KEYSTORE_FILE" ] && ANDROID_UPLOAD_KEYSTORE_FILE=$HOME/.android/debug.keystore
 [ -z "$ANDROID_UPLOAD_KEYSTORE_ALIAS" ] && ANDROID_UPLOAD_KEYSTORE_ALIAS=androiddebugkey
-PASS="--ks-pass pass:android"
-[ -n "$ANDROID_UPLOAD_KEYSTORE_PASS" ] && PASS="--ks-pass env:ANDROID_UPLOAD_KEYSTORE_PASS"
-[ -n "$ANDROID_UPLOAD_KEYSTORE_PASS_FILE" ] && PASS="--ks-pass file:$ANDROID_UPLOAD_KEYSTORE_PASS_FILE"
+#PASS="--ks-pass pass:android"
+#[ -n "$ANDROID_UPLOAD_KEYSTORE_PASS" ] && PASS="--ks-pass env:ANDROID_UPLOAD_KEYSTORE_PASS"
+#[ -n "$ANDROID_UPLOAD_KEYSTORE_PASS_FILE" ] && PASS="--ks-pass file:$ANDROID_UPLOAD_KEYSTORE_PASS_FILE"
+PASS="-storepass android"
+[ -n "$ANDROID_UPLOAD_KEYSTORE_PASS" ] && PASS="-storepass:env ANDROID_UPLOAD_KEYSTORE_PASS"
+[ -n "$ANDROID_UPLOAD_KEYSTORE_PASS_FILE" ] && PASS="-storepass:file $ANDROID_UPLOAD_KEYSTORE_PASS_FILE"
+
 
 APPNAME=termux-app
 APPVER=`grep -m 1 versionName app/build.gradle | sed 's/versionName//' | tr -d '" '`
 
-./gradlew bundleRelease || exit 1
+rm -rf tmp
+mkdir -p tmp
+export GRADLE_OPTS="-Djava.io.tmpdir=`pwd`/tmp"
 
-cd app/build/outputs/bundle/release || exit 1
+./gradlew bundleRelease
 
-# Remove old certificate
-cp -f app-release.aab ../../../../../$APPNAME-$APPVER.aab || exit 1
+#cd app/build/outputs/bundle/release || exit 1
+
 # Sign with the new certificate
-#echo Using keystore $ANDROID_UPLOAD_KEYSTORE_FILE
-#jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $ANDROID_UPLOAD_KEYSTORE_FILE $PASS ../../../../../$APPNAME-$APPVER.aab $ANDROID_UPLOAD_KEYSTORE_ALIAS || exit 1
 #apksigner sign --min-sdk-version 26 \
 # --ks $ANDROID_UPLOAD_KEYSTORE_FILE $PASS --ks-key-alias $ANDROID_UPLOAD_KEYSTORE_ALIAS \
-# ../../../../../$APPNAME-$APPVER.aab  || exit 1
+# app-release.aab || exit 1
+#jarsigner -verbose -sigalg SHA256withRSA -digestalg SHA-256 -keystore $ANDROID_UPLOAD_KEYSTORE_FILE \
+#  $PASS app-release.aab $ANDROID_UPLOAD_KEYSTORE_ALIAS || exit 1
